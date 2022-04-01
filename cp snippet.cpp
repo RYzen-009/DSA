aa5ed40298c75bcefe9c7f6e3ada49517ab11636
Tanmay Mathur <74481466+RYzen-009@users.noreply.github.com>
2022-04-01 11:53:53 +0530

    Add files via upload

---
+++ cp snippet.cpp
@@ -0,0 +1,38 @@
+	#include<bits/stdc++.h>
+	using namespace std;
+	
+	typedef long long ll;
+	typedef vector<int> v;
+	typedef vector<ll> vll;
+	typedef vector<v> vi;
+	typedef pair<int,int> pi;
+	typedef pair<long,long> pll;
+	typedef vector<pi> vpi;
+	
+	#define value first
+	#define weight second
+	#define pb push_back
+	#define eb emplace_back
+	#define d double
+	#define rep(i,x,y) for(int i=x ; i<y ; i++)
+	#define rip(j,x,y) for (int j=x; j<=y;j++)
+	
+	
+	int main()
+	{
+	  ios_base::sync_with_stdio(false);
+	  cin.tie(NULL);
+	  
+	  
+	  ll t;
+	  cin>>t;
+	  while(t--)
+	  {
+	  	
+	  	
+	    
+	  
+		
+	return 0;
+	  }
+	}		
